1ad42aabecbd74666292650daf8e538d774fed0b
Joseph Hughes <josephhughes129@gmail.com>
2020-02-10 18:40:05 -0500

    Add IEnemy Interface

---
+++ IEnemy.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Team4_LegendOfZelda
+{
+    interface IEnemy
+    {
+        void GoLeft();
+        void GoRight();
+        void GoUp();
+        void GoDown();
+        void Attack();
+        void BeHit();
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
