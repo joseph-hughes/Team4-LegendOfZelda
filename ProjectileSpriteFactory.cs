ff2234a1c43ddd1c28e94f3d7b330d182ddc5d28
Kyle Ziman <60448915+ZimanK@users.noreply.github.com>
2020-02-17 18:18:10 -0500

    Added Boomerang and magic boomerang projectile classes, updated projectile sprite factory as such and did my code reviews.

--- ProjectileSpriteFactory.cs
+++ ProjectileSpriteFactory.cs
@@ -11,6 +11,8 @@ namespace Team4_LegendOfZelda
 		private Texture2D arrowLeft;
 		private Texture2D arrowRight;
 		private Texture2D fireball;
+		private Texture2D boomerang;
+		private Texture2D magicBoomerang;
 
 
 		private static ProjectileSpriteFactory instance = new ProjectileSpriteFactory();
@@ -34,6 +36,8 @@ namespace Team4_LegendOfZelda
 			arrowLeft = content.Load<Texture2D>("ArrowLeft");
 			arrowRight = content.Load<Texture2D>("ArrowRight");
 			fireball = content.Load<Texture2D>("fireball");
+			boomerang = content.Load<Texture2D>("boomerang");
+			magicBoomerang = content.Load<Texture2D>("magicBoomerang");
 		}
 
 		public ISprite CreateArrowUpSprite()
@@ -55,6 +59,16 @@ namespace Team4_LegendOfZelda
 		public ISprite CreateFireballSprite()
 		{
 			return new TextureSprite(fireball, 1, 4, 5);
+		}
+
+		public ISprite CreateBoomerangSprite()
+		{
+			return new TextureSprite(boomerang, 1, 8, 5);
+		}
+
+		public ISprite CreateMagicBoomerangSprite()
+		{
+			return new TextureSprite(magicBoomerang, 1, 8, 5);
 		}
 	}
 }
