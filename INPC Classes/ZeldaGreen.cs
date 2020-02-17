c51aa1c9289d5edc8ddd8f22ef048149101ed265
Joseph Hughes <josephhughes129@gmail.com>
2020-02-17 14:28:52 -0500

    Add remaining NPC classes

---
+++ INPC Classes/ZeldaGreen.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Team4_LegendOfZelda.NPC_Classes
+{
+    class ZeldaGreen : INPC
+    {
+        public ISprite Sprite { get; set; }
+        public IState State { get; set; }
+        public Vector2 Position { get; set; }
+
+        public ZeldaGreen(Vector2 position)
+        {
+            Sprite = NPCSpriteFactory.Instance.CreateZeldaGreenSprite();
+            State = new NullState();
+            Position = position;
+        }
+
+        public void Update()
+        {
+            // TODO
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Sprite.Draw(spriteBatch, Position);
+        }
+    }
+}
