291db262dc0b57e5a11a9dd30b5cdbf3c1dd8ffc
su636 <58155385+su636@users.noreply.github.com>
2020-02-17 16:34:19 -0500

    Sprint2Level Updated.

--- Map Classes/Sprint2Level.cs
+++ Map Classes/Sprint2Level.cs
@@ -6,6 +6,7 @@ using Team4_LegendOfZelda.Item_Classes;
 using Team4_LegendOfZelda.ISprite_Classes;
 using Microsoft.Xna.Framework.Content;
 using Team4_LegendOfZelda.IEnemy_Classses.Bosses;
+using Team4_LegendOfZelda.IEnemy_Classses.Dungeon_Enemies;
 
 namespace Team4_LegendOfZelda.MapClasses
 {
@@ -32,7 +33,7 @@ namespace Team4_LegendOfZelda.MapClasses
         {
             
             Vector2 bcv = new Vector2(40, 40);
-            backgroundtexture = content.Load<Texture2D>("background/level1_background");
+            backgroundtexture = content.Load<Texture2D>("MapSprites/emptyroom");
 
             items = new List<Iitem>
             {
@@ -41,16 +42,59 @@ namespace Team4_LegendOfZelda.MapClasses
                 new Bomb(bcv),
                 new BookOfMagic(bcv),
                 new Bow(bcv),
+                new Clock(bcv),
+                new Compass(bcv),
+                new EmptyItem(bcv),
+                new Fairy(bcv),
+                new FiveRupies(bcv),
+                new Food(bcv),
+                new Heart(bcv),
+                new HeartContainer(bcv),
+                new Key(bcv),
+                new Letter(bcv),
+                new LifePotion(bcv),
+                new MagicalKey(bcv),
+                new MagicalRod(bcv),
+                new MagicalShield(bcv),
+                new Map(bcv),
+                new PowerBracelet(bcv),
+                new Raft(bcv),
+                new Recorder(bcv),
+                new RedCandle(bcv),
+                new RedRing(bcv),
+                new Rupy(bcv),
+                new SecondPotion(bcv),
+                new SilverArrow(bcv),
+                new Stepladder(bcv),
+                new Sword(bcv),
+                new Triforce(bcv),
+                new WhiteSword(bcv)
+
 
 
             };
-            currentItem = items[0];
+            currentItem = items[item_index];
 
             enemies = new List<IEnemy>
             {
-                
+                new Aquamentus(bcv),
+                new Dodongo(bcv),
+                new BladeTrap(bcv),
+                new GelDarkBlue(bcv),
+                new GoriyaRed(bcv),
+                new GoriyaBlue(bcv),
+                new KeeseBlue(bcv),
+                new KeeseRed(bcv),
+                new Rope(bcv),
+                new Stalfos(bcv),
+                new WallMaster(bcv),
+                new WallMasterUpsideDown(bcv),
+                new ZolDarkGreen(bcv)
+
             };
-            currentEnemy = enemies[0];
+            currentEnemy = enemies[enemy_index];
+
+
             background = new TextureSprite(backgroundtexture);
 
 
