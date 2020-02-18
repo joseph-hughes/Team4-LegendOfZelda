a8f1395ed15d4b3110160eb1841766ee4a7bf1d1
Joseph Hughes <josephhughes129@gmail.com>
2020-02-17 22:01:13 -0500

    Cleaning some stuff up

---
+++ ICommand Classes/LinkAttackCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Team4_LegendOfZelda
+{
+    public class LinkAttackCommand : ICommand
+    {
+        private IPlayer player;
+
+        public LinkAttackCommand(IPlayer player)
+        {
+            this.player = player;
+        }
+
+        public void Execute()
+        {
+            player.Attack();
+        }
+    }
+}
