fc15fd9f248f99fc4eb22cdeed7505594d3de998
neildey98 <dey.61@osu.edu>
2020-02-24 18:12:45 -0500

    added idle state methods to goriyared
    added idle state methods

--- IEnemy Classses/Dungeon Enemies/GoriyaRed States/GoriyaRedEastState.cs
+++ IEnemy Classses/Dungeon Enemies/GoriyaRed States/GoriyaRedEastState.cs
@@ -29,6 +29,10 @@
         {
             enemy.State = new GoriyaRedWestState(enemy);
         }
+        public void Idle()
+        {
+            // Do nothing
+        }
 
         public void BeDamaged()
         {
