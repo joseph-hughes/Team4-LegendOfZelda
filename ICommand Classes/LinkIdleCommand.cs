ac6d889560a35ad551c01e15209f66c1f9b82e87
neildey98 <dey.61@osu.edu>
2020-02-24 18:59:27 -0500

    Added unpressed keys function
    same

---
+++ ICommand Classes/LinkIdleCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Team4_LegendOfZelda
+{
+    public class LinkIdleCommand : ICommand
+    {
+        private IPlayer player;
+        
+        public LinkIdleCommand(IPlayer player)
+        {
+            this.player = player;
+        }
+        public void Execute()
+        {
+            player.Idle();
+        }
+    }
+}
