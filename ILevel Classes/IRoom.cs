f60276514a34936f92bd50657f401ede97572a86
Joseph Hughes <josephhughes129@gmail.com>
2020-03-01 01:01:49 -0500

    Create Room Interface
    IRoom

---
+++ ILevel Classes/IRoom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Team4_LegendOfZelda.ILevel_Classes
+{
+    public interface IRoom
+    {
+        IPlayer Player { get; set; }
+        List<IEnemy> Enemies { get; set; }
+        List<IItem> Items { get; set; }
+        List<IProjectile> Projectiles { get; set; }
+        //List<IBlock> Blocks { get; set; }
+        //List<IBoundary> Boundaries { get; set; }
+        IRoom North { get; set; }
+        IRoom East { get; set; }
+        IRoom South { get; set; }
+        IRoom West { get; set; }
+        IRoom Other { get; set; }
+
+        void Update();
+        void Draw();
+    }
+}
