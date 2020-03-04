96d2503f0520c9349257e44bdb78d36a57aa70b5
Joseph Hughes <josephhughes129@gmail.com>
2020-03-04 00:57:13 -0500

    Add HUD interface and class
    Still needs to be implemented

---
+++ ILevel Classes/DungeonHUD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Team4_LegendOfZelda.ILevel_Classes
+{
+    class DungeonHUD : IHUD
+    {
+        public void Update()
+        {
+            // TODO
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // TODO
+        }
+
+    }
+}
