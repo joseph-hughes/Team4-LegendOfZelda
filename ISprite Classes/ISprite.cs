c17ba5d5d8571b6bfeff5253edcc514973c6a9d8
Joseph Hughes <josephhughes129@gmail.com>
2020-03-04 00:56:01 -0500

    Update ISprite.cs
    Same thing about source rectangle

--- ISprite Classes/ISprite.cs
+++ ISprite Classes/ISprite.cs
@@ -5,6 +5,8 @@ namespace Team4_LegendOfZelda
 {
     public interface ISprite
     {
+        Rectangle SourceRectangle { get; set; }
+
         void Update();
         void Draw(SpriteBatch spriteBatch, Rectangle Rectangle);
     }
