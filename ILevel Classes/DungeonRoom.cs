3d3026718a013671695897f4b5ff928bf43cf41f
neildey98 <dey.61@osu.edu>
2020-03-03 21:04:06 -0500

    added xml interface
    same

---
+++ ILevel Classes/DungeonRoom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team4_LegendOfZelda.ILevel_Classes
+{
+    class DungeonRoom : IRoom
+    {
+        public IPlayer Player { get; set; }
+        System.Collections.Generic.List<IEnemy> Enemies { get; set; }
+        System.Collections.Generic.List<IItem> Items { get; set; }
+        System.Collections.Generic.List<IProjectile> Projectiles { get; set; }
+        //List<IBlock> Blocks { get; set; }
+        //List<IBoundary> Boundaries { get; set; }
+        public IRoom North { get; set; }
+        public IRoom East { get; set; }
+        public IRoom South { get; set; }
+        public IRoom West { get; set; }
+        public IRoom Other { get; set; }
+
+        public DungeonRoom()
+        { }
+        public void Update()
+        { }
+        public void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        { }
+    }
+}
---
+++ ILevel Classes/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Team4_LegendOfZelda.ILevel_Classes
+{
+    
+    class LevelLoader : IFileLoader
+
+    {
+        private XmlReader xmlReader;
+
+        public LevelLoader(string fileName)
+        {
+            xmlReader = XmlReader.Create(fileName);
+        }
+
+        public void LoadRoom()
+        {
+            IRoom room = new DungeonRoom();
+        }
+
+
+    }
+}
